011f164d536d6feeac1147f6bcdb692783e76bbd
sagarabhishek <guptaabhi920@gmail.com>
2023-05-11 19:25:32 +0530

    [all file updated]

---
+++ Crash Course/Data type and veriable/Q1.py
@@ -0,0 +1,3 @@
+num1=int(input("enter the first number: "))
+num2=int(input("enter the second number: "))
+print("sum of first and second number is : ",num1+num2)
---
+++ Crash Course/Data type and veriable/Q2.py
@@ -0,0 +1,2 @@
+num=int(input("enter the number: "))
+print("Reminder of number when divided by 2 is : ",num%2)
