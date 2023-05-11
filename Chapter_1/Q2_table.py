c67e9278db20ff5723311a94dea9a1307835a428
sagarabhishek <guptaabhi920@gmail.com>
2023-05-11 17:38:50 +0530

    Chapter 1 Completed

---
+++ Chapter_1/Q2_table.py
@@ -0,0 +1,3 @@
+table=int(input("enter the table number:"))
+for i in range(1,11):
+    print(table,"*",i,"=",table*i)
---
+++ Chapter_1/Q4_Content_of_directory.py
@@ -0,0 +1,3 @@
+# Abhishek Gupta is a data science
+import os
+print(os.listdir())
